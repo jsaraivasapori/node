b329954adc9174e5eb5d515a0590082f37fff48f
joaosaraivasapori@gmail.com <joaosaraivasapori@gmail.com>
2025-02-05 16:45:15 -0300

    aula 10 - relacionamento 1:n

--- SQL no node.js e ORMs/aula 8 - conhendo o prisma/index.js
+++ SQL no node.js e ORMs/aula 8 - conhendo o prisma/index.js
@@ -2,22 +2,39 @@ const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
 async function main() {
-  await prisma.post.createMany({
-    data: [
-      {
-        title: "Olá, mundo!",
-        content: "Este é o meu primeiro post criado com a ajuda do Prisma ORM!",
-        published: true,
+  // await prisma.post.createMany({
+  //   data: [
+  //     {
+  //       title: "Olá, mundo!",
+  //       content: "Este é o meu primeiro post criado com a ajuda do Prisma ORM!",
+  //       published: true,
+  //     },
+  //     {
+  //       title: "Post 2",
+  //       content: null,
+  //     },
+  //   ],
+  // });
+  await prisma.user.create({
+    data: {
+      name: "Isaac",
+      email: "isaac@email.com",
+      posts: {
+        create: [
+          {
+            title: "Post 1",
+            content: "Conteúdo do post 1",
+            slug: "post-1",
+          },
+        ],
       },
-      {
-        title: "Post 2",
-        content: null,
-      },
-    ],
+    },
   });
+  const allUsers = await prisma.user.findMany({
+    include: { posts: true },
+  });
+
+  console.log(allUsers, allUsers[0].posts);
 }
 
-main().then(async () => {
-  const result = await prisma.post.findMany();
-  console.table(result);
-});
+main();
