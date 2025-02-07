32ae78fd56ff5a12fa1075e50f1b3950a2fa63ff
João Vitor <joaosaraivasapori@gmail.com>
2025-02-07 16:29:09 -0300

    Criado schema de Post e sua relacao com User

---
+++ SQL no node.js e ORMs/aula 11 - operacoes basicas e exercicio 3/prisma/migrations/20250207192448_create_post_colunm/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE "Post" (
+    "id" UUID NOT NULL DEFAULT gen_random_uuid(),
+    "title" VARCHAR(255) NOT NULL,
+    "content" TEXT NOT NULL,
+    "published" BOOLEAN NOT NULL DEFAULT false,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Post_pkey" PRIMARY KEY ("id")
+);
