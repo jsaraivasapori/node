d0bcb6596441a4440d2c48f42255c7d2432c3c2d
João Vitor <joaosaraivasapori@gmail.com>
2025-02-21 19:48:34 -0300

    configuracao do dotenv e variavel de ambiente

--- SQL no node.js e ORMs/aula 12 - Projeto final/src/server.ts
+++ SQL no node.js e ORMs/aula 12 - Projeto final/src/server.ts
@@ -1,13 +1,15 @@
 import cors from "cors";
 import express from "express";
+import dotenv from "dotenv";
 import { router } from "./router";
 
+dotenv.config();
 const app = express();
 
 app.use(cors());
 app.use("/api", router);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 app.listen(PORT, () =>
   console.log(`Servidor iniciado em <http://localhost>:${PORT}/`)
 );
