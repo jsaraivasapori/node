2736145dd14ad6fae56683691d4129ff7f7fd8ab
João Vitor <joaosaraivasapori@gmail.com>
2025-02-23 20:30:38 -0300

    aula 12 finalizada

--- SQL no node.js e ORMs/aula 12 - Projeto final/src/server.ts
+++ SQL no node.js e ORMs/aula 12 - Projeto final/src/server.ts
@@ -1,12 +1,13 @@
-import cors from "cors";
-import express from "express";
-import dotenv from "dotenv";
-import { router } from "./router";
+const cors = require("cors");
+const express = require("express");
+const dotenv = require("dotenv");
+const { router } = require("./router");
 
 dotenv.config();
 const app = express();
 
 app.use(cors());
+app.use(express.json());
 app.use("/api", router);
 
 const PORT = process.env.PORT;
